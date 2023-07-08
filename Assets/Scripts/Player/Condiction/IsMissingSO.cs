34f8bffd7b5ddbe5b3af90b012d6141b08e456b3
dfdyz <2649111464@qq.com>
2023-07-08 22:50:10 +0800

    up

---
+++ Assets/Scripts/Player/Condiction/IsMissingSO.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "IsMissing", menuName = "State Machines/Conditions/Is Missing")]
+public class IsMissingSO : StateConditionSO
+{
+	protected override Condition CreateCondition() => new IsMissing();
+}
+
+public class IsMissing : Condition
+{
+	protected new IsMissingSO OriginSO => (IsMissingSO)base.OriginSO;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+	}
+	
+	protected override bool Statement()
+	{
+		return true;
+	}
+	
+	public override void OnStateEnter()
+	{
+	}
+	
+	public override void OnStateExit()
+	{
+	}
+}
